5e9ea6d2c23bbc402e14931b5389a54101dd5cd9
Дмитрий Михайлович Шишло <xlower@mail.ru>
2023-01-11 21:32:32 +1000

    Update AppConfig.cs

--- comp_app/comp_app/AppSettings/AppConfig.cs
+++ comp_app/comp_app/AppSettings/AppConfig.cs
@@ -4,11 +4,11 @@
     {
         public const string  AppVersion = "0.0.1.1";
 
-        public static string DbUserName { get; set; } = "developer3"; // имя пользователя
-        public static string DbUserPassword { get; set; } = "shishl0Dm"; // пароль пользователя
-        public static string DbPort { get; set; } = "1521"; // порт подключения  
-        public static string DbSID { get; set; } = "orcl.vladivostok.wera"; // имя сервиса 
-        public static string DbHost { get; set; } = "192.168.2.13"; // ip/хост базы 
+        public static string DbUserName { get; set; } = ""; // имя пользователя
+        public static string DbUserPassword { get; set; } = ""; // пароль пользователя
+        public static string DbPort { get; set; } = ""; // порт подключения  
+        public static string DbSID { get; set; } = ""; // имя сервиса 
+        public static string DbHost { get; set; } = ""; // ip/хост базы 
 
         public static string Schema { get; set; } = "DEVELOPER3";
         public static string connString => $"Data Source=(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = {DbHost})(PORT = {DbPort}))(CONNECT_DATA = "
